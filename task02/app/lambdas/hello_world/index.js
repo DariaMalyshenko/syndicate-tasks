e65d0d07c8a479b3b67e309d7d965a7c341d7b56
Dasha Malyshenko <maldasha2@gmail.com>
2024-08-03 00:10:48 +0300

    updated task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,17 +1,23 @@
 exports.handler = async (event) => {
     // TODO implement
-    const path = event.path;
-    const method = event.httpMethod;
+    const path = event.rawPath;
+    const method = event.requestContext.http.method;
 
     if (path === '/hello' && method === 'GET') {
         return {
             statusCode: 200,
-            message: 'Hello from Lambda',
+            body: JSON.stringify({
+                statusCode: 200,
+                message: 'Hello from Lambda'
+            }, null, 2),
         };
     } else {
         return {
-            statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            statusCode: 200,
+            body: JSON.stringify({
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            }, null, 2),
         };
     }
 };
