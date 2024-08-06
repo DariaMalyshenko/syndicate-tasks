5467d0bb1a5f942956859a4cf005a5d04f24007e
Dasha Malyshenko <maldasha2@gmail.com>
2024-08-06 20:19:52 +0300

    change

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -11,5 +11,13 @@ exports.handler = async (event) => {
                 message: 'Hello from Lambda'
             }, null, 2),
         };
-    } 
+    } else {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            }, null, 2),
+        };
+    }
 };
