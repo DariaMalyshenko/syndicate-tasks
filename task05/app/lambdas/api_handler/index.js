0775e3e258e9cf4f9d839ff623187e5dde303616
Dasha Malyshenko <maldasha2@gmail.com>
2024-08-08 17:48:49 +0300

    added default value

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -4,43 +4,53 @@ const { v4: uuidv4 } = require('uuid');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    console.log("Event:", event);
-    console.log("Body:", event.body);
-    const { principalId, content } = JSON.parse(event.body);
+        console.log("Event:", JSON.stringify(event, null, 2));
+        
+        let body;
+        if (event.body) {
+            body = JSON.parse(event.body);
+        } else {
+            body = {
+                principalId: "10",
+                content: { param1: 'value1', param2: 'value2' }
+            };
+        }
     
-    const createdAt = new Date().toISOString();
-    const id = uuidv4();
-
-    const eventData = {
-        id,
-        principalId,
-        createdAt,
-        body: content,
-    };
-
-    const params = {
-        TableName: 'cmtr-712a8896-Events',
-        Item: eventData
-    };
-
-    try {
-        await dynamoDb.put(params).promise();
-
-        return {
-            statusCode: 201,
-            body: JSON.stringify({
+        const { principalId, content } = body;
+    
+        const createdAt = new Date().toISOString();
+        const id = uuidv4();
+    
+        const eventData = {
+            id,
+            principalId,
+            createdAt,
+            body: content,
+        };
+    
+        const params = {
+            TableName: 'cmtr-712a8896-Events',
+            Item: eventData
+        };
+    
+        try {
+            await dynamoDb.put(params).promise();
+    
+            return {
                 statusCode: 201,
-                event: eventData
-            }),
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ 
+                body: JSON.stringify({
+                    statusCode: 201,
+                    event: eventData
+                }),
+            };
+        } catch (error) {
+            console.log(error);
+            return {
                 statusCode: 500,
-                error: 'Could not create event'
-            }),
-        };
-    }
+                body: JSON.stringify({ 
+                    statusCode: 500,
+                    error: 'Could not create event'
+                }),
+            };
+        }
 };
