00c57c3e6eef7898f8e1a14755fb9d41036008dd
Dasha Malyshenko <maldasha2@gmail.com>
2024-08-12 11:57:36 +0300

    task07

---
+++ task07/app/lambdas/uuid_generator/index.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+exports.handler = async (event) => {
+    console.log('Hello from Lambda');
+
+    const ids = [];
+    for (let i = 0; i < 10; i++) {
+        ids.push(uuidv4());
+    }
+
+    const resultMap = { ids };
+
+    const resultJson = JSON.stringify(resultMap);
+
+    const s3 = new AWS.S3();
+
+    const S3_OBJECT_KEY = new Date().toISOString();
+
+    const metadata = {
+        ContentLength: Buffer.byteLength(resultJson)
+    };
+
+    const inputStream = Buffer.from(resultJson);
+
+    const params = {
+        Bucket: 'cmtr-712a8896-uuid-storage-test',
+        Key: S3_OBJECT_KEY,
+        Body: inputStream,
+        Metadata: metadata
+    };
+
+    try {
+        await s3.putObject(params).promise();
+    } catch (error) {
+        console.error('Error uploading to S3:', error);
+    }
+
+    return ids;
+};
