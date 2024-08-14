9c20aaf856d6802d91721b0a4bfb627f59a76158
Dasha Malyshenko <maldasha2@gmail.com>
2024-08-14 17:14:47 +0300

    changed task08

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,8 +1,29 @@
-const OpenMeteoAPI = require('../layers/weather/weather');
+const axios = require('axios');
+class OpenMeteoAPI {
+    constructor() {
+        this.baseURL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
+    }
+
+    async getLatestForecast(latitude, longitude) {
+        try {
+            const response = await axios.get(this.baseURL, {
+                params: {
+                    latitude,
+                    longitude,
+                    current_weather: true
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            throw error;
+        }
+    }
+}
 
 exports.handler = async (event) => {
-    const latitude = 50.4375;  // Example latitude
-    const longitude = 30.5;    // Example longitude
+    const latitude = 50.4375; 
+    const longitude = 30.5;
 
     const openMeteoAPI = new OpenMeteoAPI();
 
